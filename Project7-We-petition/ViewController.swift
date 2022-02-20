4c66849ada3a1b401b9b84329fa265b81691185a
Lucas Maniero <lucasfomaniero@gmail.com>
2022-02-20 00:32:40 -0300

    Initial Commit

---
+++ Project7-We-petition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7-We-petition
+//
+//  Created by Lucas Maniero on 20/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
